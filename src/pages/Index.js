fe4d609e5ba532139fccc85484f2e61d866f9157
Kevin Solorzano <solorzke@gmail.com>
2021-01-14 19:05:43 -0500

    Retrieve data from firestore after successful authentication

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
+import { Config } from '../data/Config';
 import { Button } from 'react-bootstrap';
 import { Cookie } from '../data/Cookie';
 import '../css/login.css';
@@ -11,17 +13,8 @@ const Index = () => {
 	const history = useHistory();
 
 	useEffect(() => {
-		const config = {
-			apiKey: process.env.REACT_APP_API_KEY,
-			authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-			projectId: process.env.REACT_APP_PROJECT_ID,
-			storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-			messagingSenderId: process.env.REACT_APP_MESSENGER_SENDER_ID,
-			appId: process.env.REACT_APP_APP_ID,
-			measurementId: process.env.REACT_APP_MEASUREMENT_ID
-		};
 		//Initialize Firebase app if it hasn't already
-		firebase.apps.length === 0 ? firebase.initializeApp(config) : firebase.app();
+		firebase.apps.length === 0 ? firebase.initializeApp(Config) : firebase.app();
 	});
 
 	const authenticate = async () => {
@@ -30,6 +23,8 @@ const Index = () => {
 		let password = document.getElementById('password').value;
 		try {
 			const user = await firebase.auth().signInWithEmailAndPassword(email, password);
+			const store = await firebase.firestore().collection('users').doc(user.user.uid).get();
+			Cookie.setCookie('fname', store.data().fname);
 			Cookie.setCookie('uid', user.user.uid);
 			setProgress(false);
 			history.push('/home');
