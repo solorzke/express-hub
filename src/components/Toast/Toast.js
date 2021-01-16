17f06dd1a0ec28a43c1e00b217fdd8824ce8dadd
Kevin Solorzano <solorzke@gmail.com>
2021-01-15 22:43:31 -0500

    Toast component created with customized features

---
+++ src/components/Toast/Toast.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Toast } from 'react-bootstrap';
+import './Toast.css';
+
+const Message = ({ message, heading, img, show, onClose }) => {
+	return (
+		<div aria-live="polite" aria-atomic="true" className="toast-div">
+			<Toast className="toast-element" show={show} onClose={() => onClose}>
+				<Toast.Header>
+					{img}
+					<strong className="mr-auto">{heading}</strong>
+				</Toast.Header>
+				<Toast.Body>{message}</Toast.Body>
+			</Toast>
+		</div>
+	);
+};
+
+export default Message;
