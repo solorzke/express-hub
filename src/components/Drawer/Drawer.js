4b2e493104446f0cadb98d3d54abcc053e838b81
Kevin Solorzano <solorzke@gmail.com>
2021-01-15 22:43:11 -0500

    Drawer menu items have icons

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -8,21 +8,27 @@ const Drawer = () => {
 			<div className="sidebar-heading">Mtech Express</div>
 			<div className="list-group list-group-flush">
 				<Link to="/home" className="list-group-item list-group-item-action">
+					<i className="fas fa-home pr-2" />
 					Dashboard
 				</Link>
 				<Link to="/new-order" className="list-group-item list-group-item-action">
+					<i className="far fa-plus-square pr-2" />
 					New Order
 				</Link>
 				<Link to="/clients" className="list-group-item list-group-item-action">
+					<i className="fas fa-users pr-2" />
 					Clients
 				</Link>
 				<Link to="#" className="list-group-item list-group-item-action">
+					<i className="fas fa-search pr-2" />
 					Search
 				</Link>
 				<Link to="#" className="list-group-item list-group-item-action">
+					<i className="fas fa-cog pr-2" />
 					Settings
 				</Link>
 				<Link to="index.php?action=sign-out" className="list-group-item list-group-item-action">
+					<i className="fas fa-sign-out-alt pr-2" />
 					Sign Out
 				</Link>
 			</div>
