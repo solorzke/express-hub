998312b104c0e603e5a82f641932d48f3e8d5903
Kevin Solorzano <solorzke@gmail.com>
2021-01-20 22:29:25 -0500

    Routes for new order index leading to creating clients or orders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-
 import Index from './pages/Index';
 import Home from './pages/Home';
+import NewIndex from './pages/New/Index';
+import AddClient from './pages/New/Client';
+import AddOrder from './pages/New/Order';
 
 function App() {
 	return (
 		<Switch>
 			<Route path="/home" component={Home} />
+			<Route path="/new-order/add-client" component={AddClient} />
+			<Route path="/new-order/add-order" component={AddOrder} />
+			<Route path="/new-order" component={NewIndex} />
 			<Route path="/" component={Index} />
 		</Switch>
 	);
