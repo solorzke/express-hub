3ab622f6168675e5c0bca7867436564a4b52bbdf
Kevin Solorzano <solorzke@gmail.com>
2021-01-20 22:30:46 -0500

    Fixed bug where event handler wasnt working

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -5,7 +5,7 @@ import './Toast.css';
 const Message = ({ message, heading, img, show, onClose }) => {
 	return (
 		<div aria-live="polite" aria-atomic="true" className="toast-div">
-			<Toast className="toast-element" show={show} onClose={() => onClose}>
+			<Toast className="toast-element" show={show} onClose={onClose}>
 				<Toast.Header>
 					{img}
 					<strong className="mr-auto">{heading}</strong>
