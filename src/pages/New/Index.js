3976ffa64607192369d1188e6eb58d5596cccf76
Kevin Solorzano <solorzke@gmail.com>
2021-01-20 22:32:21 -0500

    Use anchor tags instead of Link

--- src/pages/New/Index.js
+++ src/pages/New/Index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Wrapper from '../../components/Wrapper/Wrapper';
-import { Link } from 'react-router-dom';
 import '../../css/index.css';
 
 const Index = () => <Wrapper children={<Body />} current="New Order" />;
@@ -12,24 +11,24 @@ const Body = () => {
 				<h1 className="text-center">Select An Option</h1>
 				<div className="row">
 					<div className="col text-center index-add-client-view mx-1">
-						<Link to="/new-order/add-client">
+						<a href="/new-order/add-client">
 							<i className="fas fa-user-circle index-new-user-logo pb-1" />
 							<h2>New Client</h2>
 							<p>
 								Add a new client and their shipment order, including information such as their name and
 								their shipping details to be logged into the datbase.
 							</p>
-						</Link>
+						</a>
 					</div>
 					<div className="col text-center index-add-order-view mx-1">
-						<Link to="/new-order/add-order">
+						<a href="/new-order/add-order">
 							<i className="fas fa-dolly-flatbed index-new-user-logo pb-1" />
 							<h2>New Order</h2>
 							<p>
 								Create a new order for an existing client in the database and add their shipping
 								information.
 							</p>
-						</Link>
+						</a>
 					</div>
 				</div>
 			</div>
