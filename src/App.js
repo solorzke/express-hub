913d53f2be670aa8b04eb8f12cabfad017ab34e8
Kevin Solorzano <solorzke@gmail.com>
2021-01-29 12:15:11 -0500

    404 page created to redirect if link is nonexistent

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Home from './pages/Home';
 import NewIndex from './pages/New/Index';
 import AddClient from './pages/New/Client';
 import AddOrder from './pages/New/Order';
+import AddItems from './pages/New/Items';
 import ClientIndex from './pages/Clients/Index';
 import Test from './pages/Test';
+import Error404 from './pages/404';
 
 function App() {
 	return (
@@ -14,10 +16,12 @@ function App() {
 			<Route path="/test" component={Test} />
 			<Route path="/home" component={Home} />
 			<Route path="/clients" component={ClientIndex} />
+			<Route path="/new-order/add-order/add-items" component={AddItems} />
 			<Route path="/new-order/add-client" component={AddClient} />
 			<Route path="/new-order/add-order" component={AddOrder} />
 			<Route path="/new-order" component={NewIndex} />
-			<Route path="/" component={Index} />
+			<Route exact path="/" component={Index} />
+			<Route component={Error404} />
 		</Switch>
 	);
 }
---
+++ src/pages/404.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Error404 = () => <main className="container">Error: 404 Page not found</main>;
+
+export default Error404;
