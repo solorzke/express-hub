a9dc51d054bc6cc26e98ee3882407a35720037b6
Kevin Solorzano <solorzke@gmail.com>
2021-02-05 16:51:41 -0500

    Confirm button group triggers an event to handle

---
+++ src/components/BackButton/Confirm.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const ConfirmButton = ({ onConfirm }) => {
+	//Warn the user before proceeding back to the first page that the current form data here will be lost
+	const warnBeforeProceeding = (e) => {
+		e.preventDefault();
+		const message = `Are you sure you'd like to cancel your order? All changes made will be lost.`;
+		if (window.confirm(message)) window.location.href = '/new-order';
+	};
+
+	return (
+		<div class="row pb-3">
+			<div class="col-md-12 w-100 client-pane">
+				<button class="float-sm-right btn btn-success btn-md mx-2 px-3" onClick={onConfirm}>
+					Confirm
+				</button>
+				<button
+					class="float-sm-right btn btn-secondary btn-md mx-2 px-3"
+					onClick={(e) => warnBeforeProceeding(e)}
+				>
+					Cancel
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default ConfirmButton;
