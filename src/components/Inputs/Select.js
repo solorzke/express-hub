b41cbced50b3c23584d7d5feec47a429dc2b328a
Kevin Solorzano <solorzke@gmail.com>
2021-02-11 00:48:12 -0500

    AsyncSelect comp takes props for UI rendering

---
+++ src/components/Inputs/Select.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Async from 'react-async';
+
+const AsyncSelect = ({ required = false, refs, promiseFn, formatString, onChange }) => {
+	return (
+		<div>
+			<label>Client</label>
+			<select required={required} ref={refs} className="custom-select" id="client" onChange={onChange}>
+				<option value="" disabled selected>
+					Select a client
+				</option>
+				<Async promiseFn={promiseFn} onReject={(e) => console.log(e.message)}>
+					{({ data, err, isLoading }) => {
+						if (isLoading) return 'Loading...';
+						if (err) return `Something went wrong: ${err.message}`;
+						if (data) {
+							return data.map((item, index) => {
+								let full_name = formatString(`${item.fname} ${item.lname}`);
+								return (
+									<option value={item.id} key={index}>
+										{full_name}
+									</option>
+								);
+							});
+						}
+						return null;
+					}}
+				</Async>
+			</select>
+		</div>
+	);
+};
+
+export default AsyncSelect;
