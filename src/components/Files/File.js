4c84eed534b3f0bf1e50b535e13297c9ef3b5a3d
Kevin Solorzano <solorzke@gmail.com>
2021-02-12 21:41:37 -0500

    List item that displays the order id and items

--- src/components/Files/File.js
+++ src/components/Files/File.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './File.css';
 
-const File = ({ id, date, items }) => (
-	<a href={`/clients/order?id=${id}`} className="pl-5 border border-light" style={{ backgroundColor: '#FFFCF2' }}>
-		<li>
-			Order: {id}
-			<p className="file-items text-secondary px-0">
-				<span className="file-date text-secondary pr-3">{date}</span>Items: {items}
-			</p>
-		</li>
-	</a>
-);
+const File = ({ id, date, items, names }) => {
+	return (
+		<a
+			href={`/order?id=${id}&fname=${names.fname}&lname=${names.lname}`}
+			className="pl-5 border border-light"
+			style={{ backgroundColor: '#FFFCF2' }}
+		>
+			<li>
+				Order: {id}
+				<p className="file-items text-secondary px-0">
+					<span className="file-date text-secondary pr-3">{date}</span>Items: {items}
+				</p>
+			</li>
+		</a>
+	);
+};
 
 export default File;
