236ecf4b35f085eb739187c41e4fc316c5f5dd50
Kevin Solorzano <solorzke@gmail.com>
2021-02-12 21:40:39 -0500

    Empty Component created as a placeholder comp while loading async data

---
+++ src/components/Placeholders/Empty.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Empty = () => (
+	<div className="col-md-6 d-flex flex-column justify-content-center align-items-center text-secondary">
+		<i className="fas fa-clipboard-list p-5" style={{ fontSize: '50px' }} />
+		<p>No Information Available</p>
+	</div>
+);
+
+export default Empty;
