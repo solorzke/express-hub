e1ef51e1a4a29f96a50866adec1e8ebb6798dbe9
Kevin Solorzano <solorzke@gmail.com>
2021-03-08 13:49:28 -0500

    Display row for total results found

--- src/components/Inputs/Search.js
+++ src/components/Inputs/Search.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Search.css';
 
 const SearchBar = (props) => {
+	const [ inputLength, setInputLength ] = useState(0);
+
+	const handleOnChange = (e) => {
+		props.onChange(e);
+		setInputLength(e.target.value.length);
+	};
+
 	return (
-		<div id="search">
+		<div>
 			<input
 				type="text"
 				className="form-control"
 				placeholder="Search client's name"
 				name="search"
 				id="search"
-				onChange={props.onChange}
+				onChange={handleOnChange.bind(this)}
 			/>
 			<div className="search-bar">
-				{props.data.map((item) => (
-					<a href={`/clients/${item.id}`}>
-						<p className="py-2 pl-3 my-0">{props.formatString(item.name)}</p>
+				{props.data.map((item, index) => (
+					<a href={item.path} key={index}>
+						<p className="py-2 pl-3 my-0">{props.formatString(item.detail)}</p>
 					</a>
 				))}
 			</div>
+			<ResultsCounter data={props.data} inputLength={inputLength} />
 		</div>
 	);
+};
+
+const ResultsCounter = ({ data, inputLength }) => {
+	const dataLength = data.length;
+	if (data.length > 0) {
+		return <p className="py-2 pl-3 my-0 bg-secondary text-light">{dataLength} results found</p>;
+	} else if (data.length === 0 && inputLength > 0) {
+		return <p className="py-2 pl-3 my-0 bg-secondary text-light">0 results found</p>;
+	} else {
+		return '';
+	}
 };
 
 export default SearchBar;
