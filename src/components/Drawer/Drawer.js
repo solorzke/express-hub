777e9975e0daa19b179608deed9767472db74b41
Kevin Solorzano <solorzke@gmail.com>
2021-03-08 13:48:46 -0500

    Sign out function to delete cookie session and close firebase session

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,42 +1,64 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Drawer.css';
+import Firebase from 'firebase/app';
+import 'firebase/auth';
+import { Config } from '../../data/Config';
+import { Cookie } from '../../data/Cookie';
+
+Firebase.apps.length === 0 ? Firebase.initializeApp(Config) : Firebase.app();
 
 const Drawer = ({ active = '' }) => {
+	const onSignOut = (e) => {
+		e.preventDefault();
+		Firebase.auth()
+			.signOut()
+			.then(() => {
+				console.log('> Firebase: Sign out successful');
+				Cookie.deleteCookie('fname');
+				Cookie.deleteCookie('uid');
+				window.location.href = '/';
+			})
+			.catch((error) => {
+				alert('Cannot sign out at this time. See the logs for details.');
+				console.error("> Firebase: Couldn't sign out of the session. Seek admin for help");
+				console.error(error);
+			});
+	};
+
 	return (
 		<div className="border-right" id="sidebar-wrapper">
 			<div className="sidebar-heading">Mtech Express</div>
 			<div className="list-group list-group-flush">
-				<Link to="/home" className="list-group-item list-group-item-action">
+				<a href="/home" className="list-group-item list-group-item-action">
 					<span className={active === 'dashboard' ? 'activated' : ''}>
 						<i className="fas fa-home pr-2" />Dashboard
 					</span>
-				</Link>
-				<Link to="/new-order" className="list-group-item list-group-item-action">
+				</a>
+				<a href="/new-order" className="list-group-item list-group-item-action">
 					<span className={active === 'new' ? 'activated' : ''}>
 						<i className="far fa-plus-square pr-2" />
 						New Order
 					</span>
-				</Link>
-				<Link to="/clients" className="list-group-item list-group-item-action">
+				</a>
+				<a href="/clients" className="list-group-item list-group-item-action">
 					<span className={active === 'clients' ? 'activated' : ''}>
 						<i className="fas fa-users pr-2" />Clients
 					</span>
-				</Link>
-				<Link to="#" className="list-group-item list-group-item-action">
+				</a>
+				<a href="/search" className="list-group-item list-group-item-action">
 					<span className={active === 'search' ? 'activated' : ''}>
 						<i className="fas fa-search pr-2" />Search
 					</span>
-				</Link>
-				<Link to="#" className="list-group-item list-group-item-action">
+				</a>
+				<a href="/settings" className="list-group-item list-group-item-action">
 					<span className={active === 'settings' ? 'activated' : ''}>
 						<i className="fas fa-cog pr-2" />Settings
 					</span>
-				</Link>
-				<Link to="index.php?action=sign-out" className="list-group-item list-group-item-action">
+				</a>
+				<button className="list-group-item list-group-item-action" onClick={onSignOut.bind(this)}>
 					<i className="fas fa-sign-out-alt pr-2" />
 					Sign Out
-				</Link>
+				</button>
 			</div>
 		</div>
 	);
