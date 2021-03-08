97c7787d587050ab9016aea92510ab890f57a533
Kevin Solorzano <solorzke@gmail.com>
2021-03-08 14:27:34 -0500

    Use cards to display new order options in the index page

--- src/pages/New/Index.js
+++ src/pages/New/Index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Wrapper from '../../components/Wrapper/Wrapper';
+import { Card } from 'react-bootstrap';
 import '../../css/index.css';
 
 const Index = () => <Wrapper children={<Body />} current="New Order" active="new" />;
@@ -8,32 +9,45 @@ const Body = () => {
 	return (
 		<main className="landing-page d-flex justify-content-center align-items-center index">
 			<div className="container-fluid py-5">
-				<h1 className="text-center">Select An Option</h1>
-				<div className="row">
-					<div className="col text-center index-add-client-view mx-1">
-						<a href="/new-order/add-client">
-							<i className="fas fa-user-circle index-new-user-logo pb-1" />
-							<h2>New Client</h2>
-							<p>
-								Add a new client and their shipment order, including information such as their name and
-								their shipping details to be logged into the datbase.
-							</p>
-						</a>
-					</div>
-					<div className="col text-center index-add-order-view mx-1">
-						<a href="/new-order/add-order">
-							<i className="fas fa-dolly-flatbed index-new-user-logo pb-1" />
-							<h2>New Order</h2>
-							<p>
-								Create a new order for an existing client in the database and add their shipping
-								information.
-							</p>
-						</a>
-					</div>
-				</div>
+				<h1>Select An Option</h1>
+				<CardBox
+					header="Option 1"
+					title="New Client"
+					text="Add a new client and their shipment order, including information such as their name and
+								their shipping details to be logged into the database."
+					path="/new-order/add-client"
+					image="fas fa-user-circle"
+				/>
+				<CardBox
+					header="Option 2"
+					title="New Order"
+					text="Create a new order for an existing client in the database and add their shipping
+					information."
+					path="/new-order/add-order"
+					image="fas fa-dolly-flatbed"
+				/>
 			</div>
 		</main>
 	);
 };
+
+const CardBox = ({ header, title, text, path, image }) => (
+	<a href={path}>
+		<Card className="index-add-client-view my-4">
+			<Card.Header>{header}</Card.Header>
+			<Card.Body>
+				<div className="row">
+					<div className="col-md-3 text-center ">
+						<i className={`${image} index-new-user-logo pb-1`} />
+					</div>
+					<div className="col-md-9">
+						<Card.Title>{title}</Card.Title>
+						<Card.Text>{text}</Card.Text>
+					</div>
+				</div>
+			</Card.Body>
+		</Card>
+	</a>
+);
 
 export default Index;
