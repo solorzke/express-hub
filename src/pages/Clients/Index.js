446be30b1bfded60d9bda44939438649a030023d
Kevin Solorzano <solorzke@gmail.com>
2021-03-29 12:19:38 -0400

    Erased line by accident from previous commit

--- src/pages/Clients/Index.js
+++ src/pages/Clients/Index.js
@@ -39,6 +39,7 @@ const Body = () => {
 			const data = doc.data();
 			const full_name = `${data.fname} ${data.lname}`;
 			if (full_name.includes(value) && value.length > 0) {
+				const matching = matchingResults.filter((item) => item.detail === full_name);
 				if (matching.length === 0) {
 					matchingResults.push({ detail: full_name, path: `/cloud/clients/${data.id}` });
 				}
